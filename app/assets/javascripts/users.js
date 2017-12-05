5952be97c162eded76297ec783d10f40d6f030c4
Dan Actor <danltactor@gmail.com>
2017-12-04 21:17:02 -0700

    finish get

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,32 @@
 $(document).ready( function(){
+  $('.users-display').click( function(){
+    $.ajax({
+      url: 'http://json-server.devpointlabs.com/api/v1/users',
+      method: 'GET',
+      dataType: 'JSON'
+    }).done( function(users) {
+      users.forEach( function(user){
+      var list = $('.user-list')
+      var li = '<li data-user-id="' + user.id + '"><strong>' + user.first_name + ' - ' + user.last_name + '</strong>: ' + user.phone_number + '</li>'
+      list.append(li)
+      });
+    });
+  });
 
-  $.ajax({
-    url: 'http://json-server.devpointlabs.com/api/v1/users',
-    method: 'GET',
-    dataType: 'JSON'
-  }).done( function(users){
-    console.log(users)
-  }).fail( function(users){
-    console.log('fail')
-  })
+  $('#create-user').click( function(){
+    $.ajax({
+      url: 'http://json-server.devpointlabs.com/api/v1/users',
+      method: 'POST',
+      data: {
+        user[first_name] $('#first-name')
+        user[last_name] $('#last-name')
+        user[phone_number] $('#phone-number')
+      }
+      dataType: 'JSON'
+    }).done( function(){
+      console.log("i did it")
+      alert('Done son.')
+    });
+  });
 
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,31 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render_error(@user)
+    end
   end
 
   def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render_error(@user)
+    end
   end
 
   def destroy
+    name = "#{@user.first_name} + #{@user.last_name}"
+    @user.destroy
+    render json: { message: "User #{name} Deleted"}
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone_number)
   end
 end
